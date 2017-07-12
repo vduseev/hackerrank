fd6595a787972ad82c649f45f0c59897adaf83ca
vduseev <vagiz.d@gmail.com>
2017-07-12 10:58:18 +0200

    Changed directory naming policy from dash to underscore.

--- solutions/algorithms/dynamic_programming/equal/py3/solution_01/test_answer.py
+++ solutions/algorithms/dynamic_programming/equal/py3/solution_01/test_answer.py
@@ -13,19 +13,19 @@ class TestEqual(unittest.TestCase):
         self.answerFilePath = os.path.join(os.getcwd(), 'answer.py')
 
     def test_case_0(self):
-        input_path = os.path.join(self.testCaseDir, 'tc-0.in')
-        output_path = os.path.join(self.testCaseDir, 'tc-0.out')
+        input_path = os.path.join(self.testCaseDir, 'tc_0.in')
+        output_path = os.path.join(self.testCaseDir, 'tc_0.out')
         script_output, ref_output = execute_test(self.answerFilePath, input_path, output_path)
         self.assertEqual(script_output, ref_output)
 
     def test_case_1(self):
-        input_path = os.path.join(self.testCaseDir, 'tc-1.in')
-        output_path = os.path.join(self.testCaseDir, 'tc-1.out')
+        input_path = os.path.join(self.testCaseDir, 'tc_1.in')
+        output_path = os.path.join(self.testCaseDir, 'tc_1.out')
         script_output, ref_output = execute_test(self.answerFilePath, input_path, output_path)
         self.assertEqual(script_output, ref_output)
 
     def test_case_15(self):
-        input_path = os.path.join(self.testCaseDir, 'tc-1.in')
-        output_path = os.path.join(self.testCaseDir, 'tc-1.out')
+        input_path = os.path.join(self.testCaseDir, 'tc_1.in')
+        output_path = os.path.join(self.testCaseDir, 'tc_1.out')
         script_output, ref_output = execute_test(self.answerFilePath, input_path, output_path)
         self.assertEqual(script_output, ref_output)
