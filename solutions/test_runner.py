903824cae9baaf489c89ce7715a096aa184f6652
vduseev <vagiz.d@gmail.com>
2017-07-12 10:40:28 +0200

    Modified Popen call so that subprocess is debuggable.

--- solutions/test_runner.py
+++ solutions/test_runner.py
@@ -6,31 +6,14 @@ import os
 
 
 def execute_test(solution_path, input_file_path, output_file_path):
-    # create a temporary executable based on solution source
-    # do not delete it as we'll need to make this file executable later
-    with tempfile.NamedTemporaryFile(delete=False) as executable_solution_file:
-        # get the contents of the solution script
-        with open(solution_path) as solution_file:
-            solution_text = solution_file.read()
-            # get python executable path
-            python_executable_path = sys.executable
-            # add shebang line
-            shebang_line = '#!' + python_executable_path + '\n'
-            executable_solution_file.write(shebang_line.encode('ascii'))
-            # add sys import
-            executable_solution_file.write('import sys\n'.encode('ascii'))
-            # append the rest as the contents of solution script
-            executable_solution_file.write(solution_text.encode('ascii'))
-
-    # make temporary file executable
-    current_stat = os.stat(executable_solution_file.name)
-    os.chmod(executable_solution_file.name, current_stat.st_mode | stat.S_IEXEC)
+    # get python executable path
+    python_executable_path = sys.executable
 
     # open a context for input file
     with open(input_file_path) as input_reference_file:
         # start subprocess
         answer_subprocess = subprocess.Popen(
-            executable_solution_file.name,
+            [python_executable_path, solution_path],
             stdin=input_reference_file, # serve input file context as an input
             stdout=subprocess.PIPE  # let the process print to the PIPE
         )
