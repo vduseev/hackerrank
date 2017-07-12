f845ffb66843418ba5a613587dc62095ca9d15a7
vduseev <vagiz.d@gmail.com>
2017-07-12 13:48:33 +0200

    Added solution for staircase problem.

---
+++ solutions/algorithms/warmup/staircase/python_3/solution_03_oneliner_map_to_print/answer.py
@@ -0,0 +1,1 @@
+[list(map(print,[('#'*i).rjust(n) for i in range(1,n+1)])) for n in (int(input()),)]
---
+++ solutions/algorithms/warmup/staircase/python_3/solution_05_oneliner_print_double_for/test_answer.py
@@ -0,0 +1,6 @@
+from solutions.test_case import TestCase
+
+
+class TestStaircase(TestCase):
+    def test_case_0(self):
+        self.execute_test_case()
