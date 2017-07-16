c1d317a008297682b7fc8abc5a80f5b58273251f
vduseev <vagiz.d@gmail.com>
2017-07-16 16:19:42 +0200

    Slightly better solution to coin change problem.

--- solutions/algorithms/dynamic_programming/the_coin_change_problem/python_3/solution_01/answer.py
+++ solutions/algorithms/dynamic_programming/the_coin_change_problem/python_3/solution_01/answer.py
@@ -19,23 +19,26 @@ def T(m, n):
     T(m,n) = T(m-1,n) + i*T(m,n-i*v)
     """
 
-    if n <= 0:
-        # basis of recursion
-        t = 1
-    elif m == 0 and n > 0:
-        # another basis of recursion
-        t = 0
-    elif (m, n) in memo:
+    if (m, n) in memo:
+        # value is already memoized
+        # take it from memory
         t = memo[m, n]
     else:
-        # value of current coin under index m
-        v = coins[m - 1]
-        # indicator function - returns 1 if this coins fits 1 or more times into current desired value of n
-        i = 1 if n // v > 0 else 0
-        # recursion
-        t = T(m - 1, n)
-        if i == 1:
-            t += T(m, n - v)
+        # value is not memoized
+        # compute it
+        if n <= 0:
+            # basis of recursion
+            t = 1
+        elif m == 0 and n > 0:
+            # another basis of recursion
+            t = 0
+        else:
+            # value of current coin under index m
+            v = coins[m - 1]
+            # indicator function - returns 1 if this coins fits 1 or more times into current desired value of n
+            i = 1 if n // v > 0 else 0
+            # recursion
+            t = T(m - 1, n) + i * T(m, n - v)
         # save calculated value into memo
         memo[m, n] = t
     return t
