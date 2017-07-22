df16b6f558d5f241e7e5ede2856a9753f0b23fa7
vduseev <vagiz.d@gmail.com>
2017-07-22 21:39:01 +0200

    Kingdom division solution implemented by example.
    Passes all tests except for #11.

--- solutions/algorithms/dynamic_programming/kingdom_division/python_3/solution_01/answer.py
+++ solutions/algorithms/dynamic_programming/kingdom_division/python_3/solution_01/answer.py
@@ -1,5 +1,5 @@
 MOD = 1000000007
-ADJACENT_CITIES = 0
+NEIGHBORS = 0
 PARENT = 1
 IS_LEAF = 2
 RED = 0
@@ -28,7 +28,7 @@ def scan_kingdom_map_from_input():
     fake_vertex_parent_id_value = None
     fake_is_leaf_value = False
     graph = {city_id: {
-        ADJACENT_CITIES: set(),  # set of adjacent cities
+        NEIGHBORS: set(),  # set of adjacent cities
         PARENT: fake_vertex_parent_id_value,  # parent node id
         IS_LEAF: fake_is_leaf_value  # is this node a leaf?
     } for city_id in range(1, n+1)}
@@ -39,8 +39,8 @@ def scan_kingdom_map_from_input():
         if road_start != road_end:  # Avoid loops
             # Don't forget to add road to both cities,
             # because this is an unordered graph.
-            graph[road_start][ADJACENT_CITIES].add(road_end)
-            graph[road_end][ADJACENT_CITIES].add(road_start)
+            graph[road_start][NEIGHBORS].add(road_end)
+            graph[road_end][NEIGHBORS].add(road_start)
 
     return graph
 
@@ -48,7 +48,8 @@ def scan_kingdom_map_from_input():
 def transform_graph_to_rooted_tree(graph, root):
     """
     Transforms unordered graph into roted tree by selecting 'start' vertex
-    as a root, and then assigning parents recursively.
+    as a root, and then assigning parents recursively by traversing the graph
+    with DFS algorithm.
     :param graph:
     :param root:
     :return:
@@ -62,64 +63,113 @@ def transform_graph_to_rooted_tree(graph, root):
     while stack:
         vertex = stack.pop()
         if vertex not in visited:  # Not sure if need this condition here?
+
             # We just visited a new vertex
             visited.add(vertex)
 
             # Check if this vertex is a leaf of our tree
             if vertex != root:
-                if len(graph[vertex][ADJACENT_CITIES] - set([vertex])) == 0:
+                if len(graph[vertex][NEIGHBORS] - set([vertex])) == 0:
                     graph[vertex][IS_LEAF] = True
 
             # Add all its children that were never visited to the stack.
-            unvisited_children = graph[vertex][ADJACENT_CITIES] - visited
+            unvisited_children = graph[vertex][NEIGHBORS] - visited
             stack.update(unvisited_children)
             # Record parent for these newly discovered children
             for child in unvisited_children:
                 if graph[child][PARENT] is None:  # Avoid adding parent as a child's child
                     graph[child][PARENT] = vertex
-    return visited
+
+    return graph
 
 
-def opposite_color(color):
-    if color == RED:
-        return BLUE
+def T(m, g, node, color, is_parent_same_color):
+    # Help function that returns a color opposite to the color provided
+    def opposite_color(initial_color):
+        if initial_color == RED: return BLUE
+        else: return RED
+
+    # If combinations for this key are already computed, then,
+    # obviously, don't recompute it.
+    if (node, color, is_parent_same_color) in m:
+        pass
+
+    # Leafs are the meat of these computation. They are the actual suppliers
+    # of values. For each parent number of possible combinations is equal to
+    # combinations_for_child_1 * combinations_for_child_2 * 2
+    # But when broken down to it's base this recursion actually works with leafs.
+    #
+    # However, if a leaf has a color different from its parent, such
+    # combination will not be acceptable. That's why we use 0 for this case.
+    #
+    # When leaf has same color its parent does, we consider it has 1 possible
+    # value: the same value its parent has.
+    elif g[node][IS_LEAF] is True:
+        if is_parent_same_color is False:
+            m[node, color, is_parent_same_color] = 0
+        if is_parent_same_color is True:
+            m[node, color, is_parent_same_color] = 1
+
+    # Consider usual nodes with parents and children here
     else:
-        return RED
+        current_node_combinations_count = 1
+        wrong_combinations = 1
 
-memo = {}
+        # Get a set of neighbor nodes (children) by excluding the parent from
+        # neighbor nodes set.
+        child_nodes = g[node][NEIGHBORS] - set([g[node][PARENT]])
 
+        # Consider each child
+        for child_node in child_nodes:
 
-def solve_for(graph, root, color, case):
-    if (root, color, case) in memo:
-        return memo[root, color, case]
+            # First, we compute how many combinations are there for this child
+            # if supply it with a parent with different color.
+            #
+            # The product of these possibly wrong combinations for each child
+            # will have to be subtracted from final answer of current node, if
+            # current node itself turns out to be surrounded by nodes of opposite color.
+            #
+            # Consider 2 examples (parent-to-child relationship is left-to-right;
+            # current node is the one in brackets):
+            #
+            # 1. R -> (R) -> R -> R -> R  # that's a valid combination
+            #
+            # 2. R -> (B) -> R -> R -> R  # that's a wrong combinations
+            #
+            possibly_wrong_combinations = T(
+                m, g,
+                child_node,
+                opposite_color(color),
+                is_parent_same_color=False
+            )
 
-    if graph[root][IS_LEAF] is True:
-        if case == 1:
-            memo[root, color, case] = 0
-            return 0
-        if case == 2:
-            memo[root, color, case] = 1
-            return 1
+            # We increase the amount of all possible wrong combinations
+            wrong_combinations = (wrong_combinations * possibly_wrong_combinations) % MOD
 
-    ans = 1
-    invalid = 1
+            # Second, we compute amount of combinations that are definitely valid
+            # for this child, because its parent has same color
+            valid_combinations = possibly_wrong_combinations + T(
+                m, g,
+                child_node,
+                color,
+                is_parent_same_color=True
+            )
 
-    for city in graph[root][ADJACENT_CITIES]:
-        if city != graph[root][PARENT]:
-            valid = 0
-            valid += solve_for(graph, city, opposite_color(color), 1)
-            invalid = invalid * valid
-            valid += solve_for(graph, city, color, 2)
-            ans *= valid
+            # Increase the product of all valid combinations
+            current_node_combinations_count = (current_node_combinations_count * valid_combinations) % MOD
 
-    if case == 1:
-        ans -= invalid
+        if is_parent_same_color is False:
+            current_node_combinations_count = (current_node_combinations_count - wrong_combinations + MOD) % MOD
 
-    memo[root, color, case] = ans
-    return ans
+        m[node, color, is_parent_same_color] = current_node_combinations_count
+
+    return m[node, color, is_parent_same_color]
 
 kingdom = scan_kingdom_map_from_input()
-transform_graph_to_rooted_tree(kingdom, 1)
+rooted_tree = transform_graph_to_rooted_tree(kingdom, 1)
 
-ans = (solve_for(kingdom, 1, RED, 1) * 2) % MOD
-print(ans)
+memo = {}
+root_node_id = 1
+
+answer = (T(memo, rooted_tree, root_node_id, RED, is_parent_same_color=False) * 2) % MOD
+print(answer)
--- solutions/algorithms/dynamic_programming/kingdom_division/python_3/solution_01/test_answer.py
+++ solutions/algorithms/dynamic_programming/kingdom_division/python_3/solution_01/test_answer.py
@@ -2,5 +2,11 @@ from solutions.test_case import TestCase
 
 
 class TestKingdomDivision(TestCase):
-    def test_case_explanation(self):
+    def test_case_0(self):
+        self.execute_test_case()
+
+    def test_case_1(self):
+        self.execute_test_case()
+
+    def test_case_11(self):
         self.execute_test_case()
