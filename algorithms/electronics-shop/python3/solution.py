89bc7198e72e38f1a49cb99da0dadfc2a3401be5
vduseev <vagiz.d@gmail.com>
2020-01-08 09:05:46 +0300

    Solved electronics shop in python3

---
+++ algorithms/electronics-shop/python3/solution.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    s = -1
+    for k in keyboards:
+        for d in drives:
+            combo = k + d
+            if combo <= b and combo > s:
+                s = combo
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
